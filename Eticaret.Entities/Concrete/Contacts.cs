da4bc38b0e19a671c0b41c64a2e599b01f06e913
Tuğçe Doğan <tugceedogann3@gmail.com>
2021-07-30 15:28:09 +0300

    İletişim formu eklendi.

--- Eticaret.Entities/Concrete/Contacts.cs
+++ Eticaret.Entities/Concrete/Contacts.cs
@@ -8,10 +8,11 @@ namespace Eticaret.Entities.Concrete
     public class Contacts: IEntity
     {
         public int Id { get; set; }
-        public string name { get; set; }
-        public string telefon { get; set; }
-        public string adres { get; set; }
+        public string name { get; set; }    
         public string email { get; set; }
+        public string subject { get; set; }
+        public string message { get; set; }
+
 
     }
 }
