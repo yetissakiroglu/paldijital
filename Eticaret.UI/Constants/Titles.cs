5f86413042e14e38255bdfb9fdffc859222b2df3
Yetiş Şakiroğlu <yetissakiroglu@gmail.com>
2021-08-02 08:24:59 +0300

    App Yapımı

--- Eticaret.UI/Constants/Titles.cs
+++ Eticaret.UI/Constants/Titles.cs
@@ -12,6 +12,7 @@ namespace Eticaret.UI.Constants
         public static string Radyolar = "Radyolar";
         public static string Bildir = "Hata Bildirimi";
         public static string Iletisim = "Iletisim";
+        public static string Uygulamalar = "Uygulamalar";
 
     }
 
